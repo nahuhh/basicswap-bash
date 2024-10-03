d7224b04429fb5b2ef67894c8956dc5a9910e0f4
nahuhh <50635951+nahuhh@users.noreply.github.com>
2024-10-03 03:30:32 +0000

    friendly update coincurve

--- bsx/update.sh
+++ bsx/update.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+COINCURVE=0.2
 cd $SWAP_DATADIR/basicswap
 
 # Download updated scripts
@@ -17,6 +18,18 @@ echo "Updating BasicSwapDEX" && sleep 1
 # Delete dangling build folder. Same as --no-cache for docker
 rm -rf $SWAP_DATADIR/basicswap/build
 
+# Coincurve
+cd $SWAP_DATADIR
+if [[ -d coincurve-basicswap ]]; then
+    cd coincurve-basicswap
+    git pull
+    git checkout basicswap_v$COINCURVE
+else
+    git clone https://github.com/basicswap/coincurve -b basicswap_v$COINCURVE coincurve-basicswap && cd $_
+fi
+$SWAP_DATADIR/venv/bin/pip install .
+
+# BasicSwap
 # Switch to new repo: basicswap/basicswap
 cd $SWAP_DATADIR/basicswap
 git remote set-url origin https://github.com/basicswap/basicswap
