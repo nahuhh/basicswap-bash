7f8b90fea46f2a33ed8a0f56e5f58b0daf98db38
nahuhh <50635951+nahuhh@users.noreply.github.com>
2024-11-30 12:38:34 +0000

    coins: add bitcoincash

--- bsx/auto_coinupd8.sh
+++ bsx/auto_coinupd8.sh
@@ -10,6 +10,7 @@ echo "Checking for Coin updates" && sleep 1
 
 chain=(
 bitcoin
+bitcoincash
 dash
 decred
 firo
@@ -23,22 +24,26 @@ wownero
 list=""
 select=""
 for coin in "${chain[@]}"; do
+  if [[ $coin == bitcoincash ]]; then
+    coind="bitcoin"
+  elif [[ $coin == decred ]]; then
+    coind="dcr"
+  else
+    coind="$coin"
+  fi
+
   if [[ -d $BINDIR/$coin ]]; then
-    if [[ $coin == decred ]]; then
-      UPDATE=$($BINDIR/$coin/dcrd --version | head -n 1 | grep -Fxf $SWAP_DATADIR/basicswap/core_versions)
-    else
-      UPDATE=$($BINDIR/$coin/"$coin"d --version | head -n 1 | grep -Fxf $SWAP_DATADIR/basicswap/core_versions)
-    fi
-    if [[ -z $UPDATE ]]; then
-      select+="$coin,"
-      list=${select%,}
-    fi
+    UPDATE=$($BINDIR/$coin/"$coind"d --version | head -n 1 | grep -Fxf $SWAP_DATADIR/basicswap/core_versions)
+  fi
+
+  if [[ -z $UPDATE ]]; then
+    select+="$coin,"
+    list="${select%,}"
   fi
 done
 
-echo "Updating $list"
-
 if [[ -n $select ]]; then
+  echo "Updating $list"
   . $SWAP_DATADIR/venv/bin/activate
   basicswap-prepare --datadir=$SWAP_DATADIR --preparebinonly --withcoins=$list
 else
--- bsx/removecoin.sh
+++ bsx/removecoin.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ## Prompt for user input
-printf "\n\nThe following coins can be disabled (case sensitive)\nbitcoin\ndash\ndecred\nfiro\nlitecoin\nmonero\npivx\nwownero\n\n"
+printf "\n\nThe following coins can be disabled (case sensitive)\nbitcoin\nbitcoincash\ndash\ndecred\nfiro\nlitecoin\nmonero\npivx\nwownero\n\n"
 read -p 'Full name of coin to disable [example: wownero] ' disablecoin
 ## Confirm
 printf "\nDisable $disablecoin on your BasicSwap install, correct? Press any key to continue. CTRL-C to exit\n"
