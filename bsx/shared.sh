6c4290ebdd5bcbb60d3ac3ac47ff6f4bf2a0858b
nahuhh <50635951+nahuhh@users.noreply.github.com>
2026-01-29 15:28:14 +0000

    ci: use full nodes

--- bsx/shared.sh
+++ bsx/shared.sh
@@ -11,7 +11,7 @@ export BSX_LOCAL_TOR=true          # sets host to 127.0.0.1
 export BSX_ALLOW_ENV_OVERRIDE=true # required to change the ports
 
 # Network
-[[ "${1}${2}${3}${4}" == *"regtest"* ]] && export regtest="--regtest"
+[[ "${1}${2}${3}${4}${5}" == *"regtest"* ]] && export regtest="--regtest"
 
 # Colors
 red() { echo -e "\e[31;1m$*\e[0m"; }
